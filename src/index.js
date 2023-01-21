cfa2b45b611b04535d10b4029abaed9b48369a34
Alexuber <alexuberbuber@gmail.com>
2023-01-21 16:30:00 +0200

    added new CSS for form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './saas/index.scss';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+
+const API_KEY = '33015202-198cac1ea48a9228f9ef5fb5a';
+const BASE_URL = 'https://pixabay.com/api/';
+const axios = require('axios');
+
+function getImages() {
+  return axios
+    .get(`${BASE_URL}/?key=${API_KEY}&q=flowers`)
+    .then(function (response) {
+      return response;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+}
+
+getImages().then(data => console.log(data));
