fece9b5dfd26de3d168cad293f71a6308477b95f
Alexuber <alexuberbuber@gmail.com>
2023-01-22 22:19:18 +0200

    change ax1

--- src/js/PixabayApiService.js
+++ src/js/PixabayApiService.js
@@ -4,7 +4,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const axios = require('axios').default;
 const API_KEY = '33015202-198cac1ea48a9228f9ef5fb5a';
 const BASE_URL = 'https://pixabay.com/api/';
-const axios = require('axios');
+// const axios = require('axios');
 
 const galleryEl = document.querySelector('.gallery');
 const loadMoreBtnEl = document.querySelector('.load-more');
